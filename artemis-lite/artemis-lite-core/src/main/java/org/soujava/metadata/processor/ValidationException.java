368ca6da2bf91cb3d74b4b279903a75e14b93753
otaviojava <otaviopolianasantana@gmail.com>
2020-09-27 18:05:11 -0300

    adds artemis core-sources

---
+++ artemis-lite/artemis-lite-core/src/main/java/org/soujava/metadata/processor/ValidationException.java
@@ -0,0 +1,12 @@
+package org.soujava.metadata.processor;
+
+public class ValidationException extends RuntimeException {
+
+    public ValidationException(String message) {
+        super(message);
+    }
+
+    public ValidationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
