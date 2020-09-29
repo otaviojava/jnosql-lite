dd369b1c5538bfed054e827ffbe4791e09da9e23
otaviojava <otaviopolianasantana@gmail.com>
2020-09-29 18:29:14 -0300

    adds metadataclass loader

--- artemis-lite/artemis-lite-core/src/main/java/org/eclipse/jnosql/artemis/lite/MetadataClassLoader.java
+++ artemis-lite/artemis-lite-core/src/main/java/org/eclipse/jnosql/artemis/lite/MetadataClassLoader.java
@@ -14,5 +14,55 @@
  */
 package org.eclipse.jnosql.artemis.lite;
 
+import javax.annotation.processing.Filer;
+import javax.annotation.processing.ProcessingEnvironment;
+import javax.tools.JavaFileObject;
+import java.io.*;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 final class MetadataClassLoader {
+
+    private static final String PACKAGE = "org.eclipse.jnosql.artemis.lite.metadata.";
+    private static final String METADATA = "metadata";
+    private final ProcessingEnvironment processingEnv;
+
+    MetadataClassLoader(ProcessingEnvironment processingEnv) {
+        this.processingEnv = processingEnv;
+    }
+
+    void load() throws IOException, URISyntaxException {
+        URL url = EntityProcessor.class.getClassLoader().getResource(METADATA);
+        Stream<Path> walk = Files.walk(Paths.get(url.toURI()));
+        List<String> paths = walk.collect(Collectors.toList())
+                .stream().map(Path::getFileName)
+                .map(Path::toString)
+                .filter(s -> s.contains(".java"))
+                .map(s -> s.substring(0, s.lastIndexOf(".")))
+                .collect(Collectors.toList());
+        for (String path : paths) {
+            loadClass(path);
+        }
+    }
+
+    private void loadClass(String file) throws IOException {
+        Filer filer = processingEnv.getFiler();
+        String fileName = file.substring(0, file.lastIndexOf("."));
+        JavaFileObject fileObject = filer.createSourceFile(PACKAGE + fileName);
+        try (Writer writer = fileObject.openWriter()) {
+            final InputStream stream = MetadataClassLoader.class
+                    .getClassLoader()
+                    .getResourceAsStream("ProcessorMap.java");
+            String source = new BufferedReader(new InputStreamReader(stream, StandardCharsets.UTF_8)).lines()
+                    .collect(Collectors.joining("\n"));
+            writer.append(source);
+        }
+    }
 }
