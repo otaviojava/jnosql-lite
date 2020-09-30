39c21f59ba4480e081767e9553dfc1c1596b232a
otaviojava <otaviopolianasantana@gmail.com>
2020-09-30 10:09:01 -0300

    creates Animal Test

---
+++ test/artemis-lite-core-test/src/test/java/org/eclipse/jnosql/artemis/lite/AnimalTest.java
@@ -0,0 +1,65 @@
+/*
+ *  Copyright (c) 2020 Otávio Santana and others
+ *   All rights reserved. This program and the accompanying materials
+ *   are made available under the terms of the Eclipse Public License v1.0
+ *   and Apache License v2.0 which accompanies this distribution.
+ *   The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ *   and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
+ *
+ *   You may elect to redistribute this code under either of these licenses.
+ *
+ *   Contributors:
+ *
+ *   Otavio Santana
+ */
+package org.eclipse.jnosql.artemis.lite;
+
+import org.eclipse.jnosql.artemis.lite.metadata.ClassMappings;
+import org.eclipse.jnosql.artemis.lite.metadata.EntityMetadata;
+import org.eclipse.jnosql.artemis.lite.metadata.FieldMetadata;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+
+public class AnimalTest {
+
+
+    private ClassMappings mappings;
+
+    private EntityMetadata entityMetadata;
+
+    @BeforeEach
+    public void setUp() {
+        this.mappings = new DefaultClassMappings();
+        this.entityMetadata = this.mappings.get(Animal.class);
+    }
+
+    @Test
+    public void shouldGetName() {
+        Assertions.assertEquals("kind", entityMetadata.getName());
+    }
+
+    @Test
+    public void shouldGetSimpleName() {
+        Assertions.assertEquals(Animal.class.getSimpleName(), entityMetadata.getSimpleName());
+    }
+
+    @Test
+    public void shouldGetClassName() {
+        Assertions.assertEquals(Animal.class.getName(), entityMetadata.getClassName());
+    }
+
+    @Test
+    public void shouldGetClassInstance() {
+        Assertions.assertEquals(Animal.class, entityMetadata.getClassInstance());
+    }
+
+    @Test
+    public void shouldGetId() {
+        Optional<FieldMetadata> id = this.entityMetadata.getId();
+        Assertions.assertTrue(id.isPresent());
+    }
+
+}
