e3c08300f8e59f24045da4f921c9176dab1eec30
otaviojava <otaviopolianasantana@gmail.com>
2020-09-30 11:59:48 -0300

    adds car test

---
+++ test/artemis-lite-core-test/src/test/java/org/eclipse/jnosql/artemis/lite/CarTest.java
@@ -0,0 +1,124 @@
+/*
+ *  Copyright (c) 2020 Otávio Santana and others
+ *   All rights reserved. This program and the accompanying materials
+ *   are made available under the terms of the Eclipse Public License v1.0
+ *   and Apache License v2.0 which accompanies this distribution.
+ *   The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ *   and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
+ *
+ *   You may elect to redistribute this code under either of these licenses.
+ *
+ *   Contributors:
+ *
+ *   Otavio Santana
+ */
+package org.eclipse.jnosql.artemis.lite;
+
+import org.eclipse.jnosql.artemis.lite.metadata.ClassMappings;
+import org.eclipse.jnosql.artemis.lite.metadata.DefaultClassMappings;
+import org.eclipse.jnosql.artemis.lite.metadata.EntityMetadata;
+import org.eclipse.jnosql.artemis.lite.metadata.FieldMetadata;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+public class CarTest {
+
+
+    private ClassMappings mappings;
+
+    private EntityMetadata entityMetadata;
+
+    @BeforeEach
+    public void setUp() {
+        this.mappings = new DefaultClassMappings();
+        this.entityMetadata = this.mappings.get(Car.class);
+    }
+
+    @Test
+    public void shouldGetName() {
+        Assertions.assertEquals("car", entityMetadata.getName());
+    }
+
+    @Test
+    public void shouldGetSimpleName() {
+        Assertions.assertEquals(Car.class.getSimpleName(), entityMetadata.getSimpleName());
+    }
+
+    @Test
+    public void shouldGetClassName() {
+        Assertions.assertEquals(Car.class.getName(), entityMetadata.getClassName());
+    }
+
+    @Test
+    public void shouldGetClassInstance() {
+        Assertions.assertEquals(Car.class, entityMetadata.getClassInstance());
+    }
+
+    @Test
+    public void shouldGetId() {
+        Optional<FieldMetadata> id = this.entityMetadata.getId();
+        Assertions.assertTrue(id.isPresent());
+    }
+
+    @Test
+    public void shouldCreateNewInstance() {
+        Car car = entityMetadata.newInstance();
+        Assertions.assertNotNull(car);
+        Assertions.assertTrue(car instanceof Car);
+    }
+
+    @Test
+    public void shouldGetFieldsName() {
+        List<String> fields = entityMetadata.getFieldsName();
+        Assertions.assertEquals(2, fields.size());
+        Assertions.assertTrue(fields.contains("name"));
+        Assertions.assertTrue(fields.contains("model"));
+    }
+
+    @Test
+    public void shouldGetFieldsGroupByName() {
+        Map<String, FieldMetadata> groupByName = this.entityMetadata.getFieldsGroupByName();
+        Assertions.assertNotNull(groupByName);
+        Assertions.assertNotNull(groupByName.get("_id"));
+        Assertions.assertNotNull(groupByName.get("model"));
+    }
+
+    @Test
+    public void shouldGetter() {
+        Map<String, FieldMetadata> groupByName = this.entityMetadata.getFieldsGroupByName();
+        Car car = new Car();
+        car.setModel("sport");
+        car.setName("ferrari");
+
+        String name = this.entityMetadata.getColumnField("name");
+        String model = this.entityMetadata.getColumnField("model");
+        FieldMetadata fieldName = groupByName.get(name);
+        FieldMetadata fieldModel = groupByName.get(model);
+
+        Assertions.assertEquals("sport", fieldModel.read(car));
+        Assertions.assertEquals("ferrari", fieldName.read(car));
+    }
+
+    @Test
+    public void shouldSetter() {
+        Map<String, FieldMetadata> groupByName = this.entityMetadata.getFieldsGroupByName();
+        Car car = new Car();
+
+        String name = this.entityMetadata.getColumnField("name");
+        String model = this.entityMetadata.getColumnField("model");
+        FieldMetadata fieldName = groupByName.get(name);
+        FieldMetadata fieldModel = groupByName.get(model);
+
+        fieldModel.write(car, "blue");
+        fieldName.write(car, "ada");
+        Assertions.assertEquals("blue", fieldModel.read(car));
+        Assertions.assertEquals("ada", fieldName.read(car));
+
+    }
+
+}
