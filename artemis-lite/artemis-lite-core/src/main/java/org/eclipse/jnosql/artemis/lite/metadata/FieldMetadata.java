202d2b9d02b3d55015af0e65f76f1d41078eca3b
otaviojava <otaviopolianasantana@gmail.com>
2020-10-01 16:43:58 -0300

    fixes method name with the converter

--- artemis-lite/artemis-lite-core/src/main/java/org/eclipse/jnosql/artemis/lite/metadata/FieldMetadata.java
+++ artemis-lite/artemis-lite-core/src/main/java/org/eclipse/jnosql/artemis/lite/metadata/FieldMetadata.java
@@ -37,9 +37,11 @@ public interface FieldMetadata {
     /**
      * Returns the converter instance
      * @param <X> the type of the entity attribute
+     * @param <Y> the type of the database column
+     * @param <T> the Converter
      * @return the converter if present
      */
-    <X extends AttributeConverter<?, ?>> Optional<X> getConverter();
+    <X, Y, T extends AttributeConverter<X, Y>> Optional<X> getConverter();
 
     /**
      * Returns the Java Fields name.
