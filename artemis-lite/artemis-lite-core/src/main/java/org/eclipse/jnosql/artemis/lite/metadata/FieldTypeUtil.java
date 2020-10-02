7b6f614d912c7bed8a9665ba6c75e3c2acdc9dfc
otaviojava <otaviopolianasantana@gmail.com>
2020-10-02 11:15:20 -0300

    adds new fieldtype

--- artemis-lite/artemis-lite-core/src/main/java/org/eclipse/jnosql/artemis/lite/metadata/FieldTypeUtil.java
+++ artemis-lite/artemis-lite-core/src/main/java/org/eclipse/jnosql/artemis/lite/metadata/FieldTypeUtil.java
@@ -20,11 +20,13 @@ import jakarta.nosql.mapping.Entity;
 import java.lang.reflect.Field;
 import java.util.Collection;
 import java.util.Map;
+import java.util.Optional;
 
 public final class FieldTypeUtil {
 
     private FieldTypeUtil() {
     }
+
     /**
      * select you the kind of annotation on field and then define a enum type, follow the sequences:
      * <ul>
@@ -36,14 +38,16 @@ public final class FieldTypeUtil {
      * @param field - the field with annotation
      * @return the type
      */
-    public static FieldType of(FieldMetadata field) {
+    public static FieldType of(FieldMetadata field, ClassMappings mappings) {
         if (Collection.class.isAssignableFrom(field.getType())) {
             return FieldType.COLLECTION;
         }
         if (Map.class.isAssignableFrom(field.getType())) {
             return FieldType.MAP;
         }
-        if (field.getType().isAnnotationPresent(Entity.class)) {
+        Class<?> type = field.getType();
+        Optional<EntityMetadata> entityMetadata = mappings.findByClass(type);
+        if (entityMetadata.isEmpty()) {
             return FieldType.EMBEDDED_ENTITY;
         }
 
