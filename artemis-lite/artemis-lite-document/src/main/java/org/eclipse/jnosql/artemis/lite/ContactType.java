370710804e5bcfa822c2565fce5a83bcaf3e3ac5
otaviojava <otaviopolianasantana@gmail.com>
2020-10-03 08:14:03 -0300

    adds header

--- artemis-lite/artemis-lite-document/src/main/java/org/eclipse/jnosql/artemis/lite/ContactType.java
+++ artemis-lite/artemis-lite-document/src/main/java/org/eclipse/jnosql/artemis/lite/ContactType.java
@@ -14,31 +14,6 @@
  */
 package org.eclipse.jnosql.artemis.lite;
 
-import jakarta.nosql.mapping.Column;
-import jakarta.nosql.mapping.Entity;
-
-@Entity
-public class ZipCode {
-
-    @Column
-    private String zip;
-
-    @Column
-    private String plusFour;
-
-    public String getZip() {
-        return zip;
-    }
-
-    public void setZip(String zip) {
-        this.zip = zip;
-    }
-
-    public String getPlusFour() {
-        return plusFour;
-    }
-
-    public void setPlusFour(String plusFour) {
-        this.plusFour = plusFour;
-    }
+public enum ContactType {
+    MOBILE, PHONE, EMAIL;
 }
