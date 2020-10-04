6865ce0c01bef12c94d47ae94359877703f54368
otaviojava <otaviopolianasantana@gmail.com>
2020-10-04 06:06:49 -0300

    creates worker test

---
+++ test/artemis-lite-core-test/src/test/java/org/eclipse/jnosql/artemis/lite/WorkerTest.java
@@ -0,0 +1,167 @@
+/*
+ *  Copyright (c) 2020 Otávio Santana and others
+ *   All rights reserved. This program and the accompanying materials
+ *   are made available under the terms of the Eclipse Public License v1.0
+ *   and Apache License v2.0 which accompanies this distribution.
+ *   The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ *   and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
+ *
+ *   You may elect to redistribute this code under either of these licenses.
+ *
+ *   Contributors:
+ *
+ *   Otavio Santana
+ */
+package org.eclipse.jnosql.artemis.lite;
+
+import org.eclipse.jnosql.artemis.lite.metadata.ClassMappings;
+import org.eclipse.jnosql.artemis.lite.metadata.DefaultClassMappings;
+import org.eclipse.jnosql.artemis.lite.metadata.EntityMetadata;
+import org.eclipse.jnosql.artemis.lite.metadata.FieldMetadata;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+
+public class WorkerTest {
+
+
+    private ClassMappings mappings;
+
+    private EntityMetadata entityMetadata;
+
+    @BeforeEach
+    public void setUp() {
+        this.mappings = new DefaultClassMappings();
+        this.entityMetadata = this.mappings.get(Worker.class);
+    }
+
+    @Test
+    public void shouldGetName() {
+        Assertions.assertEquals("Worker", entityMetadata.getName());
+    }
+
+    @Test
+    public void shouldGetSimpleName() {
+        Assertions.assertEquals(Worker.class.getSimpleName(), entityMetadata.getSimpleName());
+    }
+
+    @Test
+    public void shouldGetClassName() {
+        Assertions.assertEquals(Worker.class.getName(), entityMetadata.getClassName());
+    }
+
+    @Test
+    public void shouldGetClassInstance() {
+        Assertions.assertEquals(Worker.class, entityMetadata.getClassInstance());
+    }
+
+    @Test
+    public void shouldGetId() {
+        Optional<FieldMetadata> id = this.entityMetadata.getId();
+        Assertions.assertTrue(id.isPresent());
+    }
+
+    @Test
+    public void shouldCreateNewInstance() {
+        Person person = entityMetadata.newInstance();
+        Assertions.assertNotNull(person);
+        Assertions.assertTrue(person instanceof Person);
+    }
+
+    @Test
+    public void shouldGetFieldsName() {
+        List<String> fields = entityMetadata.getFieldsName();
+        Assertions.assertEquals(6, fields.size());
+        Assertions.assertTrue(fields.contains("id"));
+        Assertions.assertTrue(fields.contains("username"));
+        Assertions.assertTrue(fields.contains("email"));
+        Assertions.assertTrue(fields.contains("contacts"));
+        Assertions.assertTrue(fields.contains("pet"));
+        Assertions.assertTrue(fields.contains("salary"));
+    }
+
+    @Test
+    public void shouldGetFieldsGroupByName() {
+        Map<String, FieldMetadata> groupByName = this.entityMetadata.getFieldsGroupByName();
+        Assertions.assertNotNull(groupByName);
+        Assertions.assertNotNull(groupByName.get("_id"));
+        Assertions.assertNotNull(groupByName.get("native"));
+        Assertions.assertNotNull(groupByName.get("email"));
+        Assertions.assertNotNull(groupByName.get("contacts"));
+        Assertions.assertNotNull(groupByName.get("pet"));
+        Assertions.assertNotNull(groupByName.get("salary"));
+    }
+
+    @Test
+    public void shouldGetter() {
+        Map<String, FieldMetadata> groupByName = this.entityMetadata.getFieldsGroupByName();
+        Worker worker = new Worker();
+        worker.setId(1L);
+        worker.setUsername("otaviojava");
+        worker.setEmail("otavio@java.com");
+        worker.setContacts(List.of("Poliana", "Maria"));
+        Animal ada = new Animal();
+        ada.setName("Ada");
+        ada.setColor("black");
+        worker.setPet(ada);
+        worker.setSalary(new Money("USD", BigDecimal.TEN));
+
+        FieldMetadata id = groupByName.get("_id");
+        FieldMetadata username = groupByName.get("native");
+        FieldMetadata email = groupByName.get("email");
+        FieldMetadata contacts = groupByName.get("contacts");
+        FieldMetadata pet = groupByName.get("pet");
+        FieldMetadata salary = groupByName.get("salary");
+
+        Assertions.assertEquals(1L, id.read(worker));
+        Assertions.assertEquals("otaviojava", username.read(worker));
+        Assertions.assertEquals("otavio@java.com", email.read(worker));
+        Assertions.assertEquals(List.of("Poliana", "Maria"), contacts.read(worker));
+        Assertions.assertEquals(ada, pet.read(worker));
+        Assertions.assertEquals(new Money("USD", BigDecimal.TEN), salary.read(worker));
+    }
+
+    @Test
+    public void shouldSetter() {
+        Map<String, FieldMetadata> groupByName = this.entityMetadata.getFieldsGroupByName();
+        Worker worker = new Worker();
+        Animal ada = new Animal();
+        ada.setName("Ada");
+        ada.setColor("black");
+
+        FieldMetadata id = groupByName.get("_id");
+        FieldMetadata username = groupByName.get("native");
+        FieldMetadata email = groupByName.get("email");
+        FieldMetadata contacts = groupByName.get("contacts");
+        FieldMetadata pet = groupByName.get("pet");
+        FieldMetadata salary = groupByName.get("salary");
+
+        id.write(worker, 1L);
+        username.write(worker, "otaviojava");
+        email.write(worker, "otavio@java.com");
+        contacts.write(worker, List.of("Poliana", "Maria"));
+        pet.write(worker, ada);
+        salary.write(worker, new Money("USD", BigDecimal.TEN));
+
+        Assertions.assertEquals(1L, id.read(worker));
+        Assertions.assertEquals("otaviojava", username.read(worker));
+        Assertions.assertEquals("otavio@java.com", email.read(worker));
+        Assertions.assertEquals(List.of("Poliana", "Maria"), contacts.read(worker));
+        Assertions.assertEquals(ada, pet.read(worker));
+        Assertions.assertEquals(new Money("USD", BigDecimal.TEN), salary.read(worker));
+    }
+
+    @Test
+    public void shouldReturnGenerics() {
+        Map<String, FieldMetadata> groupByName = this.entityMetadata.getFieldsGroupByName();
+        FieldMetadata contacts = groupByName.get("contacts");
+        Set<Class<?>> arguments = contacts.getArguments();
+        Assertions.assertArrayEquals(new Class<?>[]{String.class}, arguments.toArray());
+    }
+}
