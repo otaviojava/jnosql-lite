791bdc8a8b8d3a0e03a873c9daaeafb0fe96377e
Otavio Santana <otaviopolianasantana@gmail.com>
2021-01-01 19:54:00 +0000

    creates repository processor

--- mapping-lite/mapping-lite-document/src/main/java/org/eclipse/jnosql/mapping/lite/repository/RepositoryProcessor.java
+++ mapping-lite/mapping-lite-document/src/main/java/org/eclipse/jnosql/mapping/lite/repository/RepositoryProcessor.java
@@ -1,2 +1,29 @@
-package org.eclipse.jnosql.mapping.lite.repository;public class RepositoryProcessor {
+/*
+ *  Copyright (c) 2021 Otávio Santana and others
+ *   All rights reserved. This program and the accompanying materials
+ *   are made available under the terms of the Eclipse Public License v1.0
+ *   and Apache License v2.0 which accompanies this distribution.
+ *   The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ *   and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
+ *
+ *   You may elect to redistribute this code under either of these licenses.
+ *
+ *   Contributors:
+ *
+ *   Otavio Santana
+ */
+package org.eclipse.jnosql.mapping.lite.repository;
+
+import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.RoundEnvironment;
+import javax.annotation.processing.SupportedAnnotationTypes;
+import javax.lang.model.element.TypeElement;
+import java.util.Set;
+
+@SupportedAnnotationTypes("jakarta.nosql.mapping.Entity")
+public class RepositoryProcessor extends AbstractProcessor {
+    @Override
+    public boolean process(Set<? extends TypeElement> set, RoundEnvironment roundEnvironment) {
+        return false;
+    }
 }
