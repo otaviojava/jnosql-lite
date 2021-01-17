e06d2d4cfc0f085e6ecfbbd331ff3e71a79a18e2
Otavio Santana <otaviopolianasantana@gmail.com>
2021-01-17 07:27:05 +0000

    creates factory method to methodmetadata

--- mapping-lite/mapping-lite-core/src/main/java/org/eclipse/jnosql/mapping/lite/repository/MethodMetadata.java
+++ mapping-lite/mapping-lite-core/src/main/java/org/eclipse/jnosql/mapping/lite/repository/MethodMetadata.java
@@ -14,10 +14,22 @@
  */
 package org.eclipse.jnosql.mapping.lite.repository;
 
+import javax.lang.model.element.Element;
+import javax.lang.model.element.ElementKind;
+
 public class MethodMetadata {
 
     //return
     //parameters
     //methodname
     //
+
+
+    public static MethodMetadata of(Element element) {
+        ElementKind kind = element.getKind();
+        if (ElementKind.METHOD.equals(kind)) {
+            return new MethodMetadata();
+        }
+        return null;
+    }
 }
