dbdb72b3fe5d6195aef52af385e1f9148df62ca8
Otavio Santana <otaviopolianasantana@gmail.com>
2021-01-17 07:50:22 +0000

    Updates import in Parameter

--- mapping-lite/mapping-lite-core/src/main/java/org/eclipse/jnosql/mapping/lite/repository/Parameter.java
+++ mapping-lite/mapping-lite-core/src/main/java/org/eclipse/jnosql/mapping/lite/repository/Parameter.java
@@ -17,10 +17,8 @@ package org.eclipse.jnosql.mapping.lite.repository;
 import jakarta.nosql.mapping.Param;
 
 import javax.annotation.processing.ProcessingEnvironment;
-import javax.lang.model.element.Element;
 import javax.lang.model.element.TypeElement;
 import javax.lang.model.element.VariableElement;
-import javax.lang.model.type.TypeMirror;
 
 class Parameter {
     private final String name;
