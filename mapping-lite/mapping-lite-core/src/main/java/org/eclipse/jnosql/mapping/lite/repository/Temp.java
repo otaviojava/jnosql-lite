f45830b62f62229227f8393c0e2276601097ca9b
Otavio Santana <otaviopolianasantana@gmail.com>
2021-01-17 11:18:50 +0000

    removes parambinder

--- mapping-lite/mapping-lite-core/src/main/java/org/eclipse/jnosql/mapping/lite/repository/Temp.java
+++ mapping-lite/mapping-lite-core/src/main/java/org/eclipse/jnosql/mapping/lite/repository/Temp.java
@@ -1,2 +1,26 @@
-package org.eclipse.jnosql.mapping.lite.repository;public class Temp {
+package org.eclipse.jnosql.mapping.lite.repository;
+
+import jakarta.nosql.Params;
+import jakarta.nosql.document.DocumentQueryParams;
+import jakarta.nosql.document.SelectQueryConverter;
+import jakarta.nosql.query.SelectQuery;
+import org.eclipse.jnosql.communication.document.query.SelectQueryParser;
+import org.eclipse.jnosql.communication.query.method.FindByMethodQueryProvider;
+import org.eclipse.jnosql.mapping.lite.metadata.EntityMetadata;
+import org.eclipse.jnosql.mapping.lite.metadata.RepositoryDocumentObserverParser;
+
+public class Temp {
+
+
+    public void test(MethodMetadata method, EntityMetadata entity) {
+
+        FindByMethodQueryProvider findByMethodQueryProvider = new FindByMethodQueryProvider();
+        SelectQuery query = findByMethodQueryProvider.apply(method.getMethodName(), entity.getName());
+        RepositoryDocumentObserverParser parser = new RepositoryDocumentObserverParser(null);
+        SelectQueryConverter converter = new SelectQueryParser();
+        DocumentQueryParams queryParams = converter.apply(query, parser);
+        Params params = queryParams.getParams();
+        params.bind("", "");
+
+    }
 }
