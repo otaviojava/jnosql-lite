ec7d5040c23e5e43fba676be1042dba3f454d762
Otavio Santana <otaviopolianasantana@gmail.com>
2021-01-17 11:34:43 +0000

    updates template model

--- mapping-lite/mapping-lite-core/src/main/java/org/eclipse/jnosql/mapping/lite/repository/Temp.java
+++ mapping-lite/mapping-lite-core/src/main/java/org/eclipse/jnosql/mapping/lite/repository/Temp.java
@@ -1,26 +1,36 @@
 package org.eclipse.jnosql.mapping.lite.repository;
 
 import jakarta.nosql.Params;
+import jakarta.nosql.document.DocumentQuery;
 import jakarta.nosql.document.DocumentQueryParams;
 import jakarta.nosql.document.SelectQueryConverter;
+import jakarta.nosql.mapping.document.DocumentTemplate;
 import jakarta.nosql.query.SelectQuery;
-import org.eclipse.jnosql.communication.document.query.SelectQueryParser;
 import org.eclipse.jnosql.communication.query.method.FindByMethodQueryProvider;
 import org.eclipse.jnosql.mapping.lite.metadata.EntityMetadata;
 import org.eclipse.jnosql.mapping.lite.metadata.RepositoryDocumentObserverParser;
 
+import java.util.stream.Stream;
+
 public class Temp {
 
+    private DocumentTemplate template;
 
-    public void test(MethodMetadata method, EntityMetadata entity) {
+    private EntityMetadata metadata;
 
-        FindByMethodQueryProvider findByMethodQueryProvider = new FindByMethodQueryProvider();
-        SelectQuery query = findByMethodQueryProvider.apply(method.getMethodName(), entity.getName());
-        RepositoryDocumentObserverParser parser = new RepositoryDocumentObserverParser(null);
-        SelectQueryConverter converter = new SelectQueryParser();
-        DocumentQueryParams queryParams = converter.apply(query, parser);
+    private FindByMethodQueryProvider selectProvider;
+
+    private RepositoryDocumentObserverParser parser;
+
+    private SelectQueryConverter converter;
+
+    public Stream<Object> test(MethodMetadata method) {
+        SelectQuery selectQuery = selectProvider.apply(method.getMethodName(), metadata.getName());
+        DocumentQueryParams queryParams = converter.apply(selectQuery, parser);
         Params params = queryParams.getParams();
         params.bind("", "");
+        DocumentQuery documentQuery = queryParams.getQuery();
+        return this.template.select(documentQuery);
 
     }
 }
