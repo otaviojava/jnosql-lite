3be968e0a1abde179f56f84da98c9938781b4a32
Otavio Santana <otaviopolianasantana@gmail.com>
2021-01-17 08:01:00 +0000

    updates methodmetadata constructor

--- mapping-lite/mapping-lite-core/src/main/java/org/eclipse/jnosql/mapping/lite/repository/MethodMetadata.java
+++ mapping-lite/mapping-lite-core/src/main/java/org/eclipse/jnosql/mapping/lite/repository/MethodMetadata.java
@@ -14,21 +14,49 @@
  */
 package org.eclipse.jnosql.mapping.lite.repository;
 
+import jakarta.nosql.mapping.Query;
+
+import javax.annotation.processing.ProcessingEnvironment;
 import javax.lang.model.element.Element;
 import javax.lang.model.element.ElementKind;
+import javax.lang.model.element.ExecutableElement;
+import javax.lang.model.element.TypeElement;
+import java.util.List;
+import java.util.stream.Collectors;
 
-public class MethodMetadata {
+class MethodMetadata {
 
-    //return
-    //parameters
-    //methodname
-    //
+    private final String methodName;
+    private final TypeElement returnElement;
+    private final String returnType;
+    private final List<Parameter> parameters;
+    private final Query query;
+
+    public MethodMetadata(String methodName, TypeElement returnElement, String returnType,
+                          List<Parameter> parameters, Query query) {
+
+        this.methodName = methodName;
+        this.returnElement = returnElement;
+        this.returnType = returnType;
+        this.parameters = parameters;
+        this.query = query;
+    }
 
 
-    public static MethodMetadata of(Element element) {
+    public static MethodMetadata of(Element element, ProcessingEnvironment processingEnv) {
         ElementKind kind = element.getKind();
         if (ElementKind.METHOD.equals(kind)) {
-            return new MethodMetadata();
+            ExecutableElement method = (ExecutableElement) element;
+            String methodName = method.getSimpleName().toString();
+            TypeElement returnElement = (TypeElement) processingEnv.getTypeUtils().asElement(method.getReturnType());
+            String returnType = method.getReturnType().toString();
+            List<Parameter> parameters = method.getParameters()
+                    .stream()
+                    .map(e -> Parameter.of(e, processingEnv))
+                    .collect(Collectors.toList());
+
+            Query query = method.getAnnotation(Query.class);
+            return new MethodMetadata(methodName, returnElement, returnType, parameters, query);
         }
         return null;
     }
