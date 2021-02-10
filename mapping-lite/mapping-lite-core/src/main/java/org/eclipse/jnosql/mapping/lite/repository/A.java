8b9a2ce8bbcd0577da0c8272685403a53b4a12c1
Otavio Santana <otaviopolianasantana@gmail.com>
2021-02-10 21:51:49 +0000

    adds builder to return

---
+++ mapping-lite/mapping-lite-core/src/main/java/org/eclipse/jnosql/mapping/lite/repository/A.java
@@ -0,0 +1,12 @@
+package org.eclipse.jnosql.mapping.lite.repository;
+
+import jakarta.nosql.mapping.PreparedStatement;
+
+public class A {
+
+    public void as() {
+        PreparedStatement prepare = null;
+        prepare.bind("name","name");
+        prepare.getResult()
+    }
+}
--- mapping-lite/mapping-lite-core/src/main/java/org/eclipse/jnosql/mapping/lite/repository/DocumentMethodBuilder.java
+++ mapping-lite/mapping-lite-core/src/main/java/org/eclipse/jnosql/mapping/lite/repository/DocumentMethodBuilder.java
@@ -50,6 +50,8 @@ enum DocumentMethodBuilder implements Function<MethodMetadata, List<String>> {
                     lines.add("prepare.bind(\"" + param.value() + "\"," + parameter.getName() + ")");
                 }
             }
+            MethodQueryRepositoryReturnType returnType = MethodQueryRepositoryReturnType.of(metadata);
+            lines.addAll(returnType.apply(metadata));
             return lines;
         }
     };
