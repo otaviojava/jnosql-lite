2a14fa2e58d168a3c01adc89dff63d62aacbf3e5
Otavio Santana <otaviopolianasantana@gmail.com>
2021-02-15 08:17:29 +0000

    defines lite template as application scoped

--- mapping-lite/mapping-lite-document/src/main/resources/document/LiteDocumentTemplate.java
+++ mapping-lite/mapping-lite-document/src/main/resources/document/LiteDocumentTemplate.java
@@ -35,6 +35,7 @@ import org.eclipse.jnosql.mapping.lite.metadata.DefaultClassMappings;
 import org.eclipse.jnosql.mapping.lite.metadata.EntityMetadata;
 import org.eclipse.jnosql.mapping.lite.metadata.FieldMetadata;
 
+import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import java.time.Duration;
 import java.util.Iterator;
@@ -47,6 +48,7 @@ import java.util.stream.StreamSupport;
 
 import static java.util.Objects.requireNonNull;
 
+@ApplicationScoped
 public class LiteDocumentTemplate implements DocumentTemplate {
 
     private static final DocumentQueryParser PARSER = new DefaultDocumentQueryParser();
