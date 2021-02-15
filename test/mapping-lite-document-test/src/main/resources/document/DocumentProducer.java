85e9b62c3c746dfde6303a2891734c87df205536
Otavio Santana <otaviopolianasantana@gmail.com>
2021-02-15 19:48:52 +0000

    creates document test

---
+++ test/mapping-lite-document-test/src/main/resources/document/DocumentProducer.java
@@ -0,0 +1,34 @@
+/*
+ *  Copyright (c) 2021 Otávio Santana and others
+ *   All rights reserved. This program and the accompanying materials
+ *   are made available under the terms of the Eclipse Public License v1.0
+ *   and Apache License v2.0 which accompanies this distribution.
+ *   The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ *   and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
+ *
+ *   You may elect to redistribute this code under either of these licenses.
+ *
+ *   Contributors:
+ *
+ *   Otavio Santana
+ */
+package document;
+
+import jakarta.nosql.document.DocumentCollectionManager;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.inject.Produces;
+import java.util.function.Supplier;
+
+@ApplicationScoped
+public class DocumentProducer implements Supplier<DocumentCollectionManager> {
+
+    private static final String KEY_DOCUMENT = "document";
+
+    @Override
+    @Produces
+    public DocumentCollectionManager get() {
+        DocumentCollectionConverter converter = new DocumentCollectionConverter();
+        return converter.convert(KEY_DOCUMENT);
+    }
+}
