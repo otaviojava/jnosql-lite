de070e3c729b43de250713965971cf172aa52a1b
Otavio Santana <otaviopolianasantana@gmail.com>
2021-02-17 10:56:27 +0000

    updates package name

--- mapping-lite/mapping-lite-document/src/main/java/org/eclipse/jnosql/lite/mapping/document/DocumentLiteProcessor.java
+++ mapping-lite/mapping-lite-document/src/main/java/org/eclipse/jnosql/lite/mapping/document/DocumentLiteProcessor.java
@@ -52,7 +52,7 @@ import java.util.stream.Stream;
 public class DocumentLiteProcessor extends AbstractProcessor {
 
     private static final Logger LOGGER = Logger.getLogger(DocumentLiteProcessor.class.getName());
-    private static final String PACKAGE = "org.eclipse.jnosql.mapping.document.";
+    private static final String PACKAGE = "org.eclipse.jnosql.lite.mapping.document.";
     private static final String METADATA = "document";
     private static final String TEMPLATE = "DocumentCollectionFactoryConverter.mustache";
 
