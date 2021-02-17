9e2f47f9d908a085bc12705c6429e612795d628c
Otavio Santana <otaviopolianasantana@gmail.com>
2021-02-17 09:26:11 +0000

    updates package name

--- mapping-lite/mapping-lite-document/src/main/resources/document/DocumentCollectionConverter.java
+++ mapping-lite/mapping-lite-document/src/main/resources/document/DocumentCollectionConverter.java
@@ -12,7 +12,7 @@
  *
  *   Otavio Santana
  */
-package document;
+package org.eclipse.jnosql.mapping.document;
 
 import jakarta.nosql.document.DocumentCollectionManager;
 import jakarta.nosql.document.DocumentCollectionManagerFactory;
