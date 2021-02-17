7d53d1cd37fd8baf2c30768f7913592e2eac402e
Otavio Santana <otaviopolianasantana@gmail.com>
2021-02-17 12:12:10 +0000

    updates package

--- mapping-lite/mapping-lite-core/src/main/java/org/eclipse/jnosql/lite/mapping/ClassMappingsModel.java
+++ mapping-lite/mapping-lite-core/src/main/java/org/eclipse/jnosql/lite/mapping/ClassMappingsModel.java
@@ -29,6 +29,6 @@ public class ClassMappingsModel extends BaseMappingModel {
     }
 
     public String getQualified() {
-        return "org.eclipse.jnosql.mapping.lite.metadata.DefaultClassMappings";
+        return "org.eclipse.jnosql.lite.mapping.metadataDefaultClassMappings";
     }
 }
