3fc2708df5c6011ccc6489777fc3103ffca1dbe2
Otavio Santana <otaviopolianasantana@gmail.com>
2021-02-17 18:12:47 +0000

    fixes imports

--- mapping-lite/mapping-lite-document/src/main/java/org/eclipse/jnosql/lite/mapping/document/DocumentLiteProcessor.java
+++ mapping-lite/mapping-lite-document/src/main/java/org/eclipse/jnosql/lite/mapping/document/DocumentLiteProcessor.java
@@ -14,9 +14,6 @@
  */
 package org.eclipse.jnosql.lite.mapping.document;
 
-import com.github.mustachejava.DefaultMustacheFactory;
-import com.github.mustachejava.Mustache;
-import com.github.mustachejava.MustacheFactory;
 import jakarta.nosql.mapping.MappingException;
 
 import javax.annotation.processing.AbstractProcessor;
