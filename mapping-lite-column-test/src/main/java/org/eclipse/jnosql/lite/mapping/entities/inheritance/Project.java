64192df7fee5a30adacb8cdc594d2d0b9e8b413f
Otavio Santana <otaviopolianasantana@gmail.com>
2023-07-29 17:14:37 +0100

    feat: update pom.xml
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- mapping-lite-column-test/src/main/java/org/eclipse/jnosql/lite/mapping/entities/inheritance/Project.java
+++ mapping-lite-column-test/src/main/java/org/eclipse/jnosql/lite/mapping/entities/inheritance/Project.java
@@ -14,10 +14,8 @@
  */
 package org.eclipse.jnosql.lite.mapping.entities.inheritance;
 
-import jakarta.nosql.mapping.DiscriminatorColumn;
 import jakarta.nosql.Entity;
 import jakarta.nosql.Id;
-import org.eclipse.jnosql.mapping.Inheritance;
 
 import java.util.Objects;
 
