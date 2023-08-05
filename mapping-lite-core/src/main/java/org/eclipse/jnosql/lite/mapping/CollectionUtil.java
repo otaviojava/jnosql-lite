acfaf242dcc94caf7d50fd54526f944d6587963d
Otavio Santana <otaviopolianasantana@gmail.com>
2023-08-05 13:30:25 +0100

    feat: create collection util implementation
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- mapping-lite-core/src/main/java/org/eclipse/jnosql/lite/mapping/CollectionUtil.java
+++ mapping-lite-core/src/main/java/org/eclipse/jnosql/lite/mapping/CollectionUtil.java
@@ -14,6 +14,13 @@
  */
 package org.eclipse.jnosql.lite.mapping;
 
+import java.util.Collection;
+import java.util.Deque;
+import java.util.List;
+import java.util.NavigableSet;
+import java.util.Queue;
+import java.util.Set;
+import java.util.SortedSet;
 import java.util.function.Function;
 
 enum CollectionUtil implements Function<Class<?>, String> {
@@ -21,6 +28,34 @@ enum CollectionUtil implements Function<Class<?>, String> {
 
     @Override
     public String apply(Class<?> type) {
+        if (isCollection(type)) {
+            return "new java.util.ArrayList<>()";
+        } else if (isSet(type)) {
+            return "new java.util.HashSet<>()";
+        } else if (isDeque(type)) {
+            return "new java.util.LinkedList<>()";
+        } else if (isTreeSet(type)) {
+            return "new java.util.TreeSet<>()";
+        }
         return "null";
+    }
+
+    private boolean isCollection(Class<?> type) {
+        return List.class.equals(type) ||
+                Iterable.class.equals(type)
+                || Collection.class.equals(type);
+    }
+
+    private boolean isSet(Class<?> type) {
+        return Set.class.equals(type);
+    }
+
+    private boolean isDeque(Class<?> type) {
+        return Deque.class.equals(type) || Queue.class.equals(type);
+    }
+
+    private boolean isTreeSet(Class<?> type) {
+        return NavigableSet.class.equals(type)
+                || SortedSet.class.equals(type);
     }
 }
