7b6ec9af1ee0e90c21e7fc8d231d9893d0b02f2b
Otavio Santana <otaviopolianasantana@gmail.com>
2023-08-12 04:35:10 +0100

    feat: implement lteclass scanner
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- mapping-lite-core/src/main/java/org/eclipse/jnosql/lite/mapping/LiteClassScanner.java
+++ mapping-lite-core/src/main/java/org/eclipse/jnosql/lite/mapping/LiteClassScanner.java
@@ -1,2 +1,38 @@
-package org.eclipse.jnosql.lite.mapping;public class LiteClassScanner {
+package org.eclipse.jnosql.lite.mapping;
+
+import jakarta.data.repository.DataRepository;
+import org.eclipse.jnosql.mapping.metadata.ClassScanner;
+
+import java.util.Collections;
+import java.util.Set;
+
+/**
+ * A class that implements the {@link ClassScanner} interface using the ClassGraph library for class scanning.
+ * This class provides methods to scan for different types of classes such as entities, repositories, and embeddables.
+ */
+public final class LiteClassScanner implements ClassScanner {
+    @Override
+    public Set<Class<?>> entities() {
+        return Collections.emptySet();
+    }
+
+    @Override
+    public Set<Class<?>> repositories() {
+        return Collections.emptySet();
+    }
+
+    @Override
+    public Set<Class<?>> embeddables() {
+        return Collections.emptySet();
+    }
+
+    @Override
+    public <T extends DataRepository<?, ?>> Set<Class<?>> repositories(Class<T> filter) {
+        return Collections.emptySet();
+    }
+
+    @Override
+    public Set<Class<?>> repositoriesStandard() {
+        return Collections.emptySet();
+    }
 }
