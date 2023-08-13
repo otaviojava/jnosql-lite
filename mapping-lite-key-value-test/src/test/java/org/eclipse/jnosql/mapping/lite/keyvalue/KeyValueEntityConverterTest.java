3c8b96acd4416170611b7cb375d9c70b4b8f2564
Otavio Santana <otaviopolianasantana@gmail.com>
2023-08-13 06:05:00 +0100

    test: create test to keyvalue converter
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- mapping-lite-key-value-test/src/test/java/org/eclipse/jnosql/mapping/lite/keyvalue/KeyValueEntityConverterTest.java
+++ mapping-lite-key-value-test/src/test/java/org/eclipse/jnosql/mapping/lite/keyvalue/KeyValueEntityConverterTest.java
@@ -1,8 +1,23 @@
+/*
+ *  Copyright (c) 2023 Otávio Santana and others
+ *   All rights reserved. This program and the accompanying materials
+ *   are made available under the terms of the Eclipse Public License v1.0
+ *   and Apache License v2.0 which accompanies this distribution.
+ *   The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ *   and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
+ *
+ *   You may elect to redistribute this code under either of these licenses.
+ *
+ *   Contributors:
+ *
+ *   Otavio Santana
+ */
 package org.eclipse.jnosql.mapping.lite.keyvalue;
 
 import jakarta.inject.Inject;
 import org.eclipse.jnosql.communication.Value;
 import org.eclipse.jnosql.communication.keyvalue.KeyValueEntity;
+import org.eclipse.jnosql.lite.mapping.LiteEntitiesMetadata;
 import org.eclipse.jnosql.lite.mapping.entities.Car;
 import org.eclipse.jnosql.lite.mapping.entities.Person;
 import org.eclipse.jnosql.lite.mapping.entities.Plate;
@@ -23,6 +38,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @EnableAutoWeld
 @AddPackages(value = {Converters.class, KeyValueEntityConverter.class})
+@AddPackages(LiteEntitiesMetadata.class)
 @AddExtensions({EntityMetadataExtension.class, KeyValueExtension.class})
 public class KeyValueEntityConverterTest {
 
